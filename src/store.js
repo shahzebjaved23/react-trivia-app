1235b7ed39078a596855e6339f5577e7cc441f08
Shahzeb Javed <shahzebjaved23@gmail.com>
2018-07-31 17:25:53 +0500

    minor fixes

--- src/store.js
+++ src/store.js
@@ -2,8 +2,5 @@ import { questionReducer } from './reducers/questionReducer'
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 
-const rootReducer = combineReducers({
-	questionReducer: questionReducer
-})
-
+const rootReducer = combineReducers({ questionReducer: questionReducer })
 export const store = createStore(rootReducer, applyMiddleware(thunk))
